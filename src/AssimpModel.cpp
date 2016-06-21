b51a121c84c899e546555312159a1baeef974bb4
verenuschka <verenuschka@users.noreplay.github.com>
2016-06-21 19:11:25 +0200

    Uncommented unnecessary files, so build succeeds for future Assimp integration.

--- src/AssimpModel.cpp
+++ src/AssimpModel.cpp
@@ -1,29 +1,29 @@
+////
+//// Created by Liesa  on 19/06/16.
+////
 //
-// Created by Liesa  on 19/06/16.
+//#include "AssimpModel.h"
 //
-
-#include "AssimpModel.h"
-
-bool AssimpModel::LoadModelFromFile(const char* path)
-{
-    // Create an instance of the Importer class
-    Importer importer;
-
-    // And have it read the given file with some example postprocessing
-    // Usually - if speed is not the most important aspect for you - you'll
-    // probably request more postprocessing than we do in this example.
-    const aiScene* scene = importer.ReadFile( path,
-                                              aiProcess_CalcTangentSpace       |
-                                              aiProcess_Triangulate            |
-                                              aiProcess_JoinIdenticalVertices  |
-                                              aiProcess_SortByPType);
-
-    // If the import failed, report it
-    if( !scene)
-    {
-        std::cout << "ERROR: could not import model.\n";
-        return false;
-    }
-
-    return true;
-}
+//bool AssimpModel::LoadModelFromFile(const char* path)
+//{
+//    // Create an instance of the Importer class
+//    Importer importer;
+//
+//    // And have it read the given file with some example postprocessing
+//    // Usually - if speed is not the most important aspect for you - you'll
+//    // probably request more postprocessing than we do in this example.
+//    const aiScene* scene = importer.ReadFile( path,
+//                                              aiProcess_CalcTangentSpace       |
+//                                              aiProcess_Triangulate            |
+//                                              aiProcess_JoinIdenticalVertices  |
+//                                              aiProcess_SortByPType);
+//
+//    // If the import failed, report it
+//    if( !scene)
+//    {
+//        std::cout << "ERROR: could not import model.\n";
+//        return false;
+//    }
+//
+//    return true;
+//}
